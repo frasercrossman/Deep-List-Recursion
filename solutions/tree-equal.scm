105d0ae279f17239f767006918e2f9be12e4501e
frasercrossman <frasercrossman@outlook.com>
2016-12-03 17:21:48 +0000

    Added basis case for tree-equal

--- solutions/tree-equal.scm
+++ solutions/tree-equal.scm
@@ -9,12 +9,16 @@
 
 (define tree-equal?
   (lambda (tree1 tree2)
-    (and (eqv? (car tree1) (car tree2))
-         (set-equal? (tree-labels tree1) (tree-labels tree2))
-         (or (leaf? tree1)
-             (and (node? tree1)
-             (tree-equal? (node-left tree1) (node-left tree2))
-             (tree-equal? (node-right tree1) (node-right tree2)))))))
+    (or (and (null? tree1)
+             (null? tree2))
+        (and (not (null? tree1))
+             (not (null? tree2))
+             (and (eqv? (car tree1) (car tree2))
+                  (set-equal? (tree-labels tree1) (tree-labels tree2))
+                  (or (leaf? tree1)
+                      (and (node? tree1)
+                           (tree-equal? (node-left tree1) (node-left tree2))
+                           (tree-equal? (node-right tree1) (node-right tree2)))))))))
 
 ;;; Solution Comments:
 ;;;
