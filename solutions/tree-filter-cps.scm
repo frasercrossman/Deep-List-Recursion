abdcfeb9ae5d4ec3da58b455cd93b89dc88ff96e
frasercrossman <frasercrossman@outlook.com>
2016-12-06 11:15:03 +0000

    Created file tree-filter-cps.scm

---
+++ solutions/tree-filter-cps.scm
@@ -0,0 +1,15 @@
+;;; Coursework 02
+;;; Question 05
+;;; Fraser Crossman (fc4g15@soton.ac.uk)
+;;;
+
+;;; Solution:
+
+(load "tree-implementation-list.scm")
+
+(define tree-filter-cps
+  (lambda (pred tree cont)
+    ))
+           
+;;; Solution Comments:
+;;;
