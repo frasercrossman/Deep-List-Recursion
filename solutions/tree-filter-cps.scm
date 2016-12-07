409fe11917798efbc8b53ae3fb8f57eba71b5bed
frasercrossman <frasercrossman@outlook.com>
2016-12-07 14:44:09 +0000

    Added comments for tree-filter-cps.scm

--- solutions/tree-filter-cps.scm
+++ solutions/tree-filter-cps.scm
@@ -39,5 +39,21 @@
                                        (make-node (tree-labels tree) no-left no-right))))))))))))))
 
 ;;; Solution Comments:
-;;; 
+;;; Continuation Passing Style requires that all function calls occur in
+;;; terminal position and this can be seen to be used in all three possible
+;;; cases: null, leaf, and node
+;;;
+;;; If the tree is null then the continuation is called with two empty lists.
+;;; If the tree is not null then the predicate is checked against the
+;;; labels of the tree.
+;;;   If the tree is a leaf then the continuation is called with arguments
+;;;   dependent upon the result of the predicate check.
+;;;   If the tree is a node then tree-filter-cps is called for the left node of the
+;;;   tree where the continuation is determined by calling tree-filter-cps for the
+;;;   right node of the tree and finally the continuation is called with
+;;;   arguments dependent upon the result of the predicate check. 
+;;;
+;;; This style of function execution means that the stack is not used and instead
+;;; the continuation function contains the information about how the final result
+;;; is constructed.
 ;;;
